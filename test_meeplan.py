cd588028b5c506997a26a83892f0b4a3b5a32876
Hugo Cachitas <hcachitas@gmail.com>
2015-12-10 01:03:10 +0000

    Fix typo

--- test_meeplan.py
+++ test_meeplan.py
@@ -18,7 +18,7 @@ def test_meeting():
 @pytest.fixture
 def past_meeting():
     meeting = meeplan.Meeting(
-        topic="Actvity Report",
+        topic="Activity Report",
         date=datetime.date(2015, 10, 9),
         speaker="Bill",
     )
@@ -37,8 +37,8 @@ def test_meeting_has_speaker(test_meeting):
     assert test_meeting.speaker
 
 
-# def test_available_speakers_exist():
-#     assert meeplan.available_speakers
+def test_meeting_str(past_meeting):
+    assert str(past_meeting) == "2015-10-09, Activity Report, Bill"
 
 
 def test_meeting_speaker_is_suggested():
@@ -51,6 +51,17 @@ def test_meeting_speaker_suggested_is_valid():
     meeplan.available_speakers = ["Jeb", "Bob", "Bill"]  # FIXME
     meeting = meeplan.Meeting("Crew Report", datetime.date.today())
     assert meeting.speaker in meeplan.available_speakers
+
+
+def test_speaker_suggestion_for_same_meeting_cycles_all_available_speakers():
+    meeplan.available_speakers = ["Jeb", "Bob", "Bill"]  # FIXME
+    speaker1 = meeplan.Meeting("Crew Report", datetime.date.today()).speaker
+    speaker2 = meeplan.Meeting("Crew Report", datetime.date.today()).speaker
+    assert speaker2 != speaker1
+    speaker3 = meeplan.Meeting("Crew Report", datetime.date.today()).speaker
+    assert (speaker3 != speaker1) and (speaker3 != speaker2)
+    speaker4 = meeplan.Meeting("Crew Report", datetime.date.today()).speaker
+    assert (speaker4 != speaker3) and (speaker4 != speaker2)
 
 
 # def test_meeplan_init_empty():
